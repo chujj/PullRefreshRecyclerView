b2c12c5025a4ef2957026d6e2d7d7918bbf83148
jiajun.zhu <jiajun.zhu@husor.com>
2017-10-17 19:44:37 +0800

    recommend people

--- app/src/main/java/com/ssc/weipan/login/RecommendInputActivity.java
+++ app/src/main/java/com/ssc/weipan/login/RecommendInputActivity.java
@@ -2,18 +2,28 @@ package com.ssc.weipan.login;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.EditText;
 
 import com.ssc.weipan.R;
+import com.ssc.weipan.R2;
 import com.ssc.weipan.base.BaseActivity;
 import com.ssc.weipan.base.CommonUtils;
+import com.ssc.weipan.base.ToastHelper;
 import com.ssc.weipan.base.Topbar;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by zhujj on 17-10-17.
  */
 public class RecommendInputActivity extends BaseActivity {
+
+
+
+    @BindView(R2.id.phone)
+    EditText mPhone;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,10 +35,45 @@ public class RecommendInputActivity extends BaseActivity {
 
         Topbar topbar = CommonUtils.findView(this, R.id.topbar);
         topbar.setTitle("填写推荐码");
-
-
-
     }
 
 
+
+    @OnClick(R2.id.next)
+    public void clickCommit() {
+        String recommend = mPhone.getText().toString();
+
+
+
+        if (recommend.isEmpty()) {
+            ToastHelper.showToast("请输入推荐人手机号");
+            return;
+        }
+
+
+
+        finish();
+        openHome();
+
+    }
+
+    @OnClick(R2.id.ignore)
+    public void clickIgoner() {
+
+        finish();
+        openHome();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        openHome();
+    }
+
+
+
+    public void openHome() {
+        // TODO
+    }
 }
