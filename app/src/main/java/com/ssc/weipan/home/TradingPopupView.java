044e40b45e5121abcb1066bad805e30ff64f6f84
jiajun.zhu <jiajun.zhu@husor.com>
2017-10-21 22:28:13 +0800

    trading popup layout

---
+++ app/src/main/java/com/ssc/weipan/home/TradingPopupView.java
@@ -0,0 +1,11 @@
+package com.ssc.weipan.home;
+
+import com.ssc.weipan.R;
+
+/**
+ * Created by zhujj on 17-10-21.
+ */
+public class TradingPopupView {
+
+    public static final int Layout = R.layout.trading_popup_layout;
+}
