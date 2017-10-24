083533f1a3d7c81e32c3817a4bd9f0c864f1ee91
jiajun.zhu <jiajun.zhu@husor.com>
2017-10-24 21:22:14 +0800

    trade history click toggle open status

--- app/src/main/java/com/ssc/weipan/home/TradeHistoroyAdapter.java
+++ app/src/main/java/com/ssc/weipan/home/TradeHistoroyAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ssc.weipan.R;
@@ -12,13 +13,18 @@ import com.ssc.weipan.base.ClosureMethod;
 import com.ssc.weipan.base.CommonUtils;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by zhujj on 17-10-24.
  */
 public class TradeHistoroyAdapter extends BaseAdapter {
     private List<GoodsApi.BuyTradeData> mData = new ArrayList<>();
+
+
+    private Set<GoodsApi.BuyTradeData> mSelectedData = new HashSet<>();
 
     @Override
     public int getCount() {
@@ -37,6 +43,7 @@ public class TradeHistoroyAdapter extends BaseAdapter {
 
     public void setData(List<GoodsApi.BuyTradeData> open) {
         mData = open;
+        mSelectedData.clear();
     }
 
     @Override
@@ -53,13 +60,33 @@ public class TradeHistoroyAdapter extends BaseAdapter {
 //            final TextView trade_detail = CommonUtils.findView(root, R.id.trade_detail);
 
 
+            final ImageView toggleImage = CommonUtils.findView(root, R.id.open_toggle);
+            final View toggleContainer = CommonUtils.findView(root, R.id.toggle_container);
+            final View detailContainer = CommonUtils.findView(root, R.id.detail_container);
+
             root.setTag(new ClosureMethod() {
                 @Override
                 public Object[] run(Object... args) {
-                    GoodsApi.BuyTradeData data = (GoodsApi.BuyTradeData) args[0];
+                    final GoodsApi.BuyTradeData data = (GoodsApi.BuyTradeData) args[0];
+
 
 
                     name.setText(data.goods_name);
+
+                    toggleContainer.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (mSelectedData.contains(data)) {
+                                mSelectedData.remove(data);
+                            } else {
+                                mSelectedData.add(data);
+                            }
+                            TradeHistoroyAdapter.this.notifyDataSetChanged();
+                        }
+                    });
+                    toggleImage.setImageResource(mSelectedData.contains(data) ?
+                            R.drawable.edu_ic_funflat_arrow_down : R.drawable.edu_ic_funflat_arrow_up);
+                    detailContainer.setVisibility(mSelectedData.contains(data) ? View.VISIBLE : View.GONE);
 
 
                     return new Object[0];
