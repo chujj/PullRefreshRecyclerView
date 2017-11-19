78637c9d49fe08ee50f8d93300108c234fb2dd67
jiajun.zhu <jiajun.zhu@husor.com>
2017-11-19 17:15:25 +0800

    order 的ui

--- app/src/main/java/com/biaoyixin/shangcheng/home/OrderActivity.java
+++ app/src/main/java/com/biaoyixin/shangcheng/home/OrderActivity.java
@@ -1,9 +1,21 @@
 package com.biaoyixin.shangcheng.home;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.biaoyixin.shangcheng.R;
 import com.biaoyixin.shangcheng.base.BaseActivity;
 
 /**
  * Created by zhujj on 17-11-19.
  */
 public class OrderActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        this.setContentView(R.layout.order_activity);
+    }
 }
