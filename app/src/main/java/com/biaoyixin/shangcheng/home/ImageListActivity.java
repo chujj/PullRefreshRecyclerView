fb837964a7cfcbb74e8d611035033d1d30cf5b27
jiajun.zhu <jiajun.zhu@husor.com>
2017-11-19 14:53:47 +0800

    帮助的入口界面

--- app/src/main/java/com/biaoyixin/shangcheng/home/ImageListActivity.java
+++ app/src/main/java/com/biaoyixin/shangcheng/home/ImageListActivity.java
@@ -1,13 +1,18 @@
 package com.biaoyixin.shangcheng.home;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.biaoyixin.shangcheng.R;
 import com.biaoyixin.shangcheng.R2;
 import com.biaoyixin.shangcheng.base.BaseActivity;
 import com.biaoyixin.shangcheng.base.Topbar;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -36,5 +41,38 @@ public class ImageListActivity extends BaseActivity {
         this.setContentView(R.layout.image_list_activity);
 
         ButterKnife.bind(this, this);
+
+        for (int i = 0; i < mImages.length; i++) {
+
+            if (mImages[i].startsWith("assets://")) {
+                loadFromAssets(mImages[i].substring("assets://".length()));
+            }
+
+        }
+    }
+
+    private void loadFromAssets(String substring) {
+        final ImageView imageView = new ImageView(this);
+        imageView.setBackgroundColor(0xffff0000);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        try {
+            final int screenWidth = getResources().getDisplayMetrics().widthPixels;
+
+            Bitmap bitmap = BitmapFactory.decodeStream(
+                    getResources().getAssets().open(substring));
+
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+
+            int computedHeight = (int) (1f * height / width * screenWidth);
+            lp.height = computedHeight;
+
+            imageView.setImageBitmap(bitmap);
+            mImagesContainer.addView(imageView, lp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
